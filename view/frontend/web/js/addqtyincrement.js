70665be35d297bcfa5823df0894cfd0ce1c4ec01
Gabriel Silva <gabriel.silva@me.com>
2017-03-01 01:38:13 +0000

    Adjustments

--- view/frontend/web/js/addqtyincrement.js
+++ view/frontend/web/js/addqtyincrement.js
@@ -1,5 +1,7 @@
 require(['jquery', 'jquery/ui'], function($) {
-    $('.inc.button').click(function() {
+    $('.inc.button').click(function(e) {
+        e.preventDefault();
+
         var $this = $(this);
         var $input = $this.prev('input');
         var increment = parseInt($this.attr('ref'));;
@@ -11,8 +13,15 @@ require(['jquery', 'jquery/ui'], function($) {
         var newValue = parseInt($input.val()) + increment;
 
         $input.val(newValue);
+
+        if (!$input.is(':visible')) {
+            // var buttonContent = "Add <strong style='font-size:2rem; border: 1px solid white;'>" + newValue + "</strong> to Cart";
+            $this.parent().next('.qty.add').find('button > span > strong').html(newValue);
+        }
     });
-    $('.dec.button').click(function() {
+    $('.dec.button').click(function(e) {
+        e.preventDefault();
+
         var $this = $(this);
         var $input = $this.next('input');
         var increment = parseInt($this.attr('ref'));
@@ -23,8 +32,31 @@ require(['jquery', 'jquery/ui'], function($) {
 
         var newValue = parseInt($input.val()) - increment;
 
-        if (newValue >= 1) {
-            $input.val(newValue);
+        if (newValue < 1) {
+            newValue = 1;
+        }
+
+        $input.val(newValue);
+
+        if (!$input.is(':visible')) {
+            // var buttonContent = "Add <strong style='font-size:2rem; border: 1px solid white;'>" + newValue + "</strong> to Cart";
+            $this.parent().next('.qty.add').find('button > span > strong').html(newValue);
         }
     });
+    $('.qty.add > button > span').bind("DOMSubtreeModified",function(){
+        if ($(this).html() == "Add to Cart") {
+            var inputValue = $(this).parent().parent().parent().find('input[name=qty]').val();
+            var buttonContent = "Add <strong  class='text-qty-list'>" + inputValue + "</strong> to Cart";
+
+            $(this).html(buttonContent);
+        }
+    });
+
+
+    // change(function() {
+    //     alert('TEST');
+    //     // if ($(this).html() == "Add to Cart") {
+    //     //     $(this).css('border', '1px solid red');
+    //     // }
+    // });
 });
